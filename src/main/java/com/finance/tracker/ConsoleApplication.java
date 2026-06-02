99fc040de3def3c61bd9c7e76a3e3c5ca851e5a0
Gabriel Vieira <gabrielvieira040@outlook.com>
2026-06-01 22:52:30 -0300

    feat: Inicio do bloco de registrar uma transação.

--- src/main/java/com/finance/tracker/ConsoleApplication.java
+++ src/main/java/com/finance/tracker/ConsoleApplication.java
@@ -1,6 +1,7 @@
 package com.finance.tracker;
 
 import java.util.Scanner;
+import java.util.List;
 import com.finance.tracker.model.*;
 import com.finance.tracker.repository.*;
 import com.finance.tracker.service.*;
@@ -12,27 +13,56 @@ public class ConsoleApplication {
         TransactionRepository transactionRepository = new InMemoryTransactionRepository();
         TransactionService transactionService = new TransactionService(transactionRepository);
         CategoryRepository categoryRepository = new InMemoryCategoryRepository();
+        CategoryService categoryService = new CategoryService(categoryRepository);
 
         while (true) {
             int mainMenu;
             System.out.println("=== Rastreador de Finanças Pessoais ===");
             System.out.println("1. Adicionar Transação (Receita/Despesa)");
-            System.out.println("2. Listar Todas as Transações");
-            System.out.println("3. Visualizar Saldo e Resumo");
-            System.out.println("4. Sair");
+            System.out.println("2. Adicionar nova Categoria");
+            System.out.println("3. Listar Todas as Transações");
+            System.out.println("4. Visualizar Saldo e Resumo");
+            System.out.println("5. Sair");
             System.out.println("Escolha uma opção: ");
 
             try {
                 mainMenu = Integer.parseInt(sc.nextLine());
-                if (mainMenu >= 1 && mainMenu <= 4) {
+                if (mainMenu >= 1 && mainMenu <= 5) {
                     switch (mainMenu) {
                         case 1:
+                            boolean newTransaction = false;
+                            while (!newTransaction) {
+                                try {
+                                    System.out.println("\n--- Cadastrar Nova Transação ---");
+                                    System.out.print("Digite o ID da transação: ");
+                                    int id = Integer.parseInt(sc.nextLine());
+
+                                    System.out.print("Digite o valor: R$ ");
+                                    double value = Double.parseDouble(sc.nextLine());
+
+                                    System.out.print("Digite a descrição: ");
+                                    String desc = sc.nextLine();
+
+                                    System.out.print("Qual o tipo de Transação? (1 - Receita, 2 - Despesa): ");
+                                    int option = Integer.parseInt(sc.nextLine());
+                                    if (option != 1 && option != 2) {
+                                        throw new IllegalArgumentException(
+                                                "A opção desejada não é válida. Por favor, tente novamente.");
+                                    }
+                                    TransactionType type = (option == 1) ? TransactionType.REVENUE
+                                            : TransactionType.EXPENSES;
+                                } catch (Exception e) {
+                                    System.out.println("\nErro: " + e.getMessage() + " Tente novamente.");
+                                }
+                            }
                             break;
                         case 2:
                             break;
                         case 3:
                             break;
                         case 4:
+                            break;
+                        case 5:
                             break;
                     }
                 } else {
